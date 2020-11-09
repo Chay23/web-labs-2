2468eb5b8d3a8e33c003fc5dada53be1c7a6f117
Nazar Shcherbiy <Chay23@users.noreply.github.com>
2020-11-10 01:20:58 +0200

    Lab-4

--- lab_2/app/forms.py
+++ lab_2/app/forms.py
@@ -1,9 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
+# Regexp
+#  Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+#                                               'Username must have only '
+#                                               'letters, numbers, dots or '
+#                                               'underscores')])
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Sign In')
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign in')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[Length(min=4, max=25,
+                                                          message="Username length should be between 4 and 25"),
+                                                   DataRequired(message="This field can't be empty")])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6, message="Length of password should be greater than 6")])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sing up')
--- lab_2/app/views.py
+++ lab_2/app/views.py
@@ -1,7 +1,14 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app import bcrypt
+from app.forms import LoginForm, RegistrationForm
+from .models import User, Post
+
+
+# @app.route('/main')
+# def index():
+#     return render_template('index.html', name='Інженерія програмного забезпечення', title='PNU')
 
 
 @app.route('/')
@@ -20,11 +27,41 @@ def second_work():
     return render_template('second_work.html')
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/posts')
+def posts():
+    posts = Post.query.all()
+    return render_template('posts.html', posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login for user {} {}'.format(
-            form.username.data, form.password.data))
-        return redirect(url_for('login'))
+        email = form.email.data
+        password = form.password.data
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and bcrypt.check_password_hash(user.password_hash, password):
+            flash("Sing in successfully")
+            return redirect(url_for('main'))
+        else:
+            flash('Login or password is incorrect', 'warning')
     return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+
+        hashed = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        user = User(username=username, email=email, password_hash=hashed)
+        db.session.add(user)
+        db.session.commit()
+        flash("Sing up successfully")
+    return render_template('register.html', form=form)
