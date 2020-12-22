8d886c7bd3bb08ebebba69e6f689732b94c67e94
Nazar Shcherbiy <Chay23@users.noreply.github.com>
2020-12-23 01:57:26 +0200

    fixed db data init

--- lab_2/flask_database.py
+++ lab_2/flask_database.py
@@ -1,6 +1,6 @@
 from flask_script import Manager, prompt_bool, Command
 
-from app import db
+from app import db, bcrypt
 from app.models import User, Post, Post_API
 
 manager = Manager(usage="Perform database operations")
@@ -26,7 +26,8 @@ def recreate():
 
 @manager.command
 def init_data():
-    u = User(username="User", email="example@mail.com", password_hash="pass1234", admin=True)
+    hashed = bcrypt.generate_password_hash('pass1234').decode('utf-8')
+    u = User(username="User", email="example@mail.com", password_hash=hashed, admin=True)
     db.session.add(u)
     p = Post(title="Title", body="Post example", author=u)
     p_api = Post_API(title="Title", body="Post example", user_id=u.id)
